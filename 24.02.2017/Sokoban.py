f3ac02f961049ac6778f8a88325362bbb4b5a2bf
Nguyen Viet Trung <trungnv.1710@gmail.com>
2017-02-27 12:46:37 +0700

    last update

--- 24.02.2017/Sokoban.py
+++ 24.02.2017/Sokoban.py
@@ -1,14 +1,3 @@
-px = 1
-py = 2
-bx = 2
-by = 2
-cx = 4
-cy = 3
-
-screen_width = 5
-screen_height = 5
-
-
 def in_map(x, y, screen_width, screen_height):
     if x < 0 or y < 0 or x > screen_width - 1 or y > screen_height - 1:
         return False
@@ -55,16 +44,26 @@ def touch_box(px, py, bx, by):
 
 def pushing(px, py, bx, by, choice):
     if(bx == px):
-        if(by - py == 1) and (choice == 's'):
+        if(by - py == 1) and (choice == 'S'):
             return True
-        if(py - by == 1) and (choice == 'w'):
+        if(py - by == 1) and (choice == 'W'):
             return True
     if(by == py):
-        if(px - bx == 1) and (choice == 'a'):
+        if(px - bx == 1) and (choice == 'A'):
             return True
-        if(bx - px == 1) and (choice == 'd'):
+        if(bx - px == 1) and (choice == 'D'):
             return True
     return False
+
+px = 1
+py = 2
+bx = 2
+by = 2
+cx = 4
+cy = 3
+
+screen_width = 8
+screen_height = 8
 
 while True:
     for y in range(screen_height):
@@ -94,7 +93,7 @@ while True:
         dx = 1
 
     [next_px, next_py] = move(px, py, dx, dy)
-    if (pushing(px, py, bx, by, choice)):
+    if pushing(px, py, bx, by, choice):
         [next_bx, next_by] = move(bx, by, dx, dy)
     else:
         next_bx = bx
